c8fc6f7404fc45095ed0a2182c7ac3675ba0094b
sriky <srikanthbm@gmail.com>
2018-04-29 15:15:13 -0700

    chore: Add empty Android Project

---
+++ app/src/main/java/com/sriky/nytimesmoviereview/MovieReviewActivity.java
@@ -0,0 +1,13 @@
+package com.sriky.nytimesmoviereview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_review);
+    }
+}
